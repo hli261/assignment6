498bf80cd38102990056ba1275fedae748587eb6
hli261 <50968650+hli261@users.noreply.github.com>
2020-08-13 16:53:02 -0400

    heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/web422-a4'));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/web422-a4/' });
+});
+
+app.listen(process.env.PORT || 8080);
